0b16779246b6ab52ba7ba2d24b6e335ef68710aa
Mohammadreza <77621358+MRAlirad@users.noreply.github.com>
2024-11-20 12:38:58 +0330

    Compiling Smart Contracts

--- src/SimpleStorage.sol
+++ src/SimpleStorage.sol
@@ -1,50 +1,33 @@
+// I'm a comment!
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8; // 0.8.7 and newer version
+
+pragma solidity 0.8.19;
+
+// pragma solidity ^0.8.0;
+// pragma solidity >=0.8.0 <0.9.0;
 
 contract SimpleStorage {
-    uint favouriteNumber = 10;
+    uint256 myFavoriteNumber;
 
-    // string name is being mapped to uint256 favouriteNumber;
-    mapping(string => uint256) public nameToFavouriteNumber;
-
-    People public person = People({favouriteNumber: 10, name: "John"});
-
-    struct People {
-        uint256 favouriteNumber;
+    struct Person {
+        uint256 favoriteNumber;
         string name;
     }
+    // uint256[] public anArray;
+    Person[] public listOfPeople;
 
-    function store(uint256 _favouriteNumber) public {
-        favouriteNumber = _favouriteNumber;
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
     }
 
     function retrieve() public view returns (uint256) {
-        return favouriteNumber;
+        return myFavoriteNumber;
     }
 
-    function add() public pure returns (uint256) {
-        return (1 + 1);
-    }
-
-    function addPerson(string memory _name, uint256 _favouriteNumber) public {
-        //! way number 1
-        // people.push(Person(_favouriteNumber, _name));
-
-        //! Way number 2
-        // Person memory newPerson = Person({
-        //     favouriteNumber: _favouriteNumber,
-        //     name: _name
-        // });
-        // people.push(newPerson);
-
-        //! Way number 3
-        // Person memory newPerson = Person(_favouriteNumber, _name);
-        // people.push(newPerson);
-
-        //! Way number 4 - the best in my openion
-        people.push(Person({favouriteNumber: _favouriteNumber, name: _name}));
-
-        // mapping
-        nameToFavouriteNumber[_name] = _favouriteNumber;
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
